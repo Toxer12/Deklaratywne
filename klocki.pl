5d8e53e6feb30e47d297fbde0dec89b2b33515db
Toxer12 <115220195+Toxer12@users.noreply.github.com>
2024-02-28 14:13:38 +0100

    Add files via upload

---
+++ klocki.pl
@@ -0,0 +1,25 @@
+% PROGRAM: klocki_1
+% Baza wiedzy o ukladzie klockow
+% Definiowane predykaty:
+% 	na/2
+%=================================
+
+% na(X,Y)
+% opis: spelniony, gdy klocek X lezy bezposrednio na klocku Y
+% ------------------------------ na/2
+	na(c,a).
+	na(c,b).
+	na(d,c).
+	pod(a,b):-na(b,a).
+	miedzy(X,Y,Z):-na(X,Y),pod(X,Z).
+	miedzy(X,Y,Z):-na(X,Z),pod(X,Y).
+% ------------------------------ na/2
+
+	pod(X,Y) :- na(Y,X).
+
+/*
+	3 klauzul
+	1 definicja relacji
+	relacja na/2
+	relacja na/2: 3 klauzul, ktore sa faktami
+*/
