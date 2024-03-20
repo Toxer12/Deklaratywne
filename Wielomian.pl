ea381fc7f2a521bb41468f74795066dbc4d4e9a2
Toxer12 <115220195+Toxer12@users.noreply.github.com>
2024-03-20 14:43:51 +0100

    Add files via upload

---
+++ Wielomian.pl
@@ -0,0 +1,15 @@
+%rozpoznanie wielomianu o jednej zmiennej
+
+	wielomian(X,X).
+	wielomian(C,_):-number(C).
+
+	wielomian(-W,X):-wielomian(W,X).
+	wielomian(W1+W2,X):-wielomian(W1,X),
+	wielomian(W2,X).
+	wielomian(W1-W2,X):-wielomian(W1,X),
+	wielomian(W2,X).
+	wielomian(W1*W2,X):-wielomian(W1,X),
+	wielomian(W2,X).
+	wielomian(W^N,X):-integer(N),
+	N>1,
+	wielomian(W,X).
